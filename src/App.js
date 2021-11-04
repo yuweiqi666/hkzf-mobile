79f554e9797389f8be773ade01de2e3696db7136
yuweiqi <1171020517@qq.com>
2021-11-04 22:44:23 +0800

    全局样式引入 路由配置引入 antd-mobile引入

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+
+import Home from './pages/home'
+
+import CityList from './pages/cityList'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='App'>
+            
+            
+            
+            <Link to='/home'>首页</Link>
+            <Link to='/cityList'>城市列表</Link>
+
+            <Switch>
+              <Route path='/home' component={ Home } />
+              <Route path='/cityList' component={ CityList } />
+            </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
---
+++ src/pages/cityList/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class CityList extends Component {
+
+  render() {
+    return(
+      <div>
+        i am cityList
+      </div>
+    )
+  }
+
+
+}
